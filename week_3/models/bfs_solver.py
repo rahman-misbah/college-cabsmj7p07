a562c3a451ee6e32e2f4b4650c560d6c3d329944
Misbahur Rahman <rahman.misbah2604@gmail.com>
2026-08-28 18:07:05 +0530

    feat: update type hints

--- week_3/models/bfs_solver.py
+++ week_3/models/bfs_solver.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 from . import ProblemBase
 from data_structures import Queue
 
-class BFSSolver:
+class BFSSolver[T]:
     def __init__(self, maze_problem:ProblemBase, early_exit:bool = True):
-        self._maze_problem = maze_problem
-        self._early_exit = early_exit
+        self._maze_problem : ProblemBase = maze_problem
+        self._early_exit : bool = early_exit
     
     # PROPERTIES ----------------------------------------------------------------------------------
 
@@ -19,15 +21,15 @@ class BFSSolver:
     # PRIVATE METHODS -----------------------------------------------------------------------------
 
     def _solve_bfs(self):
-        parent_dict:dict = dict()
-        goal_states:set = set()
-        queue = Queue()
+        parent_dict:dict[T, Optional[T]] = dict()
+        goal_states:set[T] = set()
+        queue : Queue = Queue()
 
         queue.enqueue(self.maze_problem.initial_state)
         parent_dict[self.maze_problem.initial_state] = None
 
         while not queue.is_empty():
-            current_state = queue.dequeue()
+            current_state:T = queue.dequeue()
 
             if self.maze_problem.goal_test(current_state):
                 goal_states.add(current_state)
@@ -35,6 +37,7 @@ class BFSSolver:
                 if self.early_exit:
                     return parent_dict, goal_states
             
+            next_state:T
             for next_state in self.maze_problem.actions(current_state):
                 if next_state not in parent_dict:
                     parent_dict[next_state] = current_state
@@ -42,12 +45,12 @@ class BFSSolver:
         
         return parent_dict, goal_states
     
-    def _construct_solutions(self, parent_dict, goal_states):
-        solution_list = list()
+    def _construct_solutions(self, parent_dict:dict[T, Optional[T]], goal_states:set[T]):
+        solution_list:list[list[T]] = list()
 
         for goal in goal_states:
-            current_solution = list()
-            current_state = goal
+            current_solution:list[T] = list()
+            current_state[T] = goal
 
             while current_state is not None:
                 current_solution.append(current_state)
