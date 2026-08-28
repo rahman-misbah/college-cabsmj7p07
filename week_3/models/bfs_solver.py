fef16feeb36fccecf890c463609e97ea6fd937d7
Misbahur Rahman <rahman.misbah2604@gmail.com>
2026-08-28 18:22:04 +0530

    fix: final final type hints fix

--- week_3/models/bfs_solver.py
+++ week_3/models/bfs_solver.py
@@ -11,7 +11,7 @@ class BFSSolver[T]:
     # PROPERTIES ----------------------------------------------------------------------------------
 
     @property
-    def maze_problem(self) -> ProblemBase:
+    def problem(self) -> ProblemBase[T]:
         return self._problem
     
     @property
@@ -20,32 +20,35 @@ class BFSSolver[T]:
     
     # PRIVATE METHODS -----------------------------------------------------------------------------
 
-    def _solve_bfs(self):
+    def _solve_bfs(self) -> tuple[dict[T, Optional[T]], set[T]]:
         parent_dict:dict[T, Optional[T]] = dict()
         goal_states:set[T] = set()
         queue : Queue = Queue()
 
-        queue.enqueue(self.maze_problem.initial_state)
-        parent_dict[self.maze_problem.initial_state] = None
+        queue.enqueue(self.problem.initial_state)
+        parent_dict[self.problem.initial_state] = None
 
         while not queue.is_empty():
             current_state:T = queue.dequeue()
 
-            if self.maze_problem.goal_test(current_state):
+            if self.problem.goal_test(current_state):
                 goal_states.add(current_state)
 
                 if self.early_exit:
                     return parent_dict, goal_states
             
             next_state:T
-            for next_state in self.maze_problem.actions(current_state):
+            for next_state in self.problem.actions(current_state):
                 if next_state not in parent_dict:
                     parent_dict[next_state] = current_state
                     queue.enqueue(next_state)
         
         return parent_dict, goal_states
     
-    def _construct_solutions(self, parent_dict:dict[T, Optional[T]], goal_states:set[T]):
+    def _construct_solutions(self, 
+                             parent_dict:dict[T, Optional[T]], 
+                             goal_states:set[T]
+                             ) -> list[list[T]]:
         solution_list:list[list[T]] = list()
 
         for goal in goal_states:
@@ -66,7 +69,7 @@ class BFSSolver[T]:
     def toggle_early_exit(self) -> None:
         self._early_exit = not self._early_exit
     
-    def solve(self):
+    def solve(self) -> list[list[T]]:
         parent_dict, goal_states = self._solve_bfs()
         solutions = self._construct_solutions(parent_dict, goal_states)
 
