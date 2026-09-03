df112558fb4172e89724abf505d1134d94d98df8
Misbahur Rahman <rahman.misbah2604@gmail.com>
2026-09-03 15:20:01 +0530

    feat: expose solver bases

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from .cell import Cell
 from .maze import Maze
 from .problem_base import TraverseProblemBase, GoalProblemBase
+from .solver_base import TraverseSolverBase, GoalSolverBase
 from .bfs_solver import BFSSolver
 from .dfs_solver import DFSGoalSolver
 
@@ -8,7 +9,9 @@ __all__ = [
     Cell,
     Maze,
     TraverseProblemBase,
+    GoalProblemBase,
+    TraverseSolverBase,
+    GoalSolverBase,
     BFSSolver,
-    DFSGoalSolver,
-    GoalProblemBase
+    DFSGoalSolver
 ]
